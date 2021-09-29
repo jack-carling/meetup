49f9a87e3f4d4e92e5ee18934dedca5714ada62b
jack-carling <jack@carling.se>
2021-09-29 10:01:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Meetup
+
+## Project setup
+
+```bash
+# Install dependencies
+npm install
+
+# Run development server
+npm run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
